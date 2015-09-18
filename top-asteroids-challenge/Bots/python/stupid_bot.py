fb81073a9db103c9d32f87d633e6361240723848
Marco Prado <marco.pdsv@gmail.com>
2015-09-18 13:52:57 -0300

    First commit

---
+++ top-asteroids-challenge/Bots/python/stupid_bot.py
@@ -0,0 +1,16 @@
+from bot_interface import *
+import math
+
+
+
+class StupidBot(BotBase):
+
+    def process(self, gamestate):
+
+        return Action(1, 1, 1, 1)
+
+
+
+
+
+GameState(StupidBot()).connect()
---
+++ top-asteroids-challenge/README.md
@@ -0,0 +1,5 @@
+# top-asteroids-challenge
+
+Para mais informações veja wiki:
+
+https://github.com/topfreegames/top-asteroids-challenge/wiki
